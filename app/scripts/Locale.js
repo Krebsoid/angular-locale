1a4704d369d0194de3de856d74360b5208759fe3
Malte Bruweleit <malte.bruweleit@e-7.com>
2014-09-01 17:21:53 +0200

    bower config adjusted again / added locale template directive

--- app/scripts/Locale.js
+++ app/scripts/Locale.js
@@ -3,24 +3,24 @@
 angular.module('LocaleModule', ['LocalStorageModule'])
 
 
-    .controller('LocaleController',['$scope','Locale', function($scope, Locale) {
+    .controller('LocaleController', ['$scope', 'Locale', function ($scope, Locale) {
         $scope.language = Locale.activeLocale;
 
-        $scope.changeLocale = function(locale) {
+        $scope.changeLocale = function (locale) {
             Locale.changeLocale(locale || $scope.language);
             $scope.language = Locale.activeLocale;
         };
     }])
 
-    .directive('localeWrapper', ['Locale', function(Locale) {
+    .directive('localeWrapper', ['Locale', function (Locale) {
         return {
-            link:function (scope, element) {
+            link: function (scope, element) {
                 scope.language = Locale.getLanguage();
                 scope.$watch(
-                    function() {
+                    function () {
                         return Locale.getLanguage();
                     },
-                    function(l1, l2) {
+                    function (l1, l2) {
                         element.removeClass(l2);
                         element.addClass(l1);
                     }
@@ -29,23 +29,23 @@ angular.module('LocaleModule', ['LocalStorageModule'])
         };
     }])
 
-    .directive('localeString', ['Locale', function(Locale) {
+    .directive('localeString', ['Locale', function (Locale) {
         return {
-            restrict:'A',
-            scope:{
-                localeString:'@localeString',
-                localeVariables:'=localeVariables'
+            restrict: 'A',
+            scope: {
+                localeString: '@localeString',
+                localeVariables: '=localeVariables'
             },
-            link:function (scope, element, attrs) {
+            link: function (scope, element, attrs) {
                 scope.isTemplate = !!scope.localeVariables;
                 angular.extend(scope, attrs.localeVariables);
 
                 scope.$watch(
-                    function() {
+                    function () {
                         return Locale.getLocale();
                     },
-                    function(locale) {
-                        if(!scope.isTemplate) {
+                    function (locale) {
+                        if (!scope.isTemplate) {
                             scope.outputString = Object.byString(Localization, attrs.localeString)[locale];
                         }
                         else {
@@ -54,12 +54,31 @@ angular.module('LocaleModule', ['LocalStorageModule'])
                     }
                 );
             },
-            template:'<span data-ng-bind-html="outputString"></span>'
+            template: '<span data-ng-bind-html="outputString"></span>'
         };
     }])
 
-    .factory('Locale',['$location', 'localStorageService', function($location, LocalStorage) {
-        return (function() {
+    .directive('localeTemplate', ['Locale', function (Locale) {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: true,
+            link: function (scope, element, attrs) {
+                scope.$watch(
+                    function () {
+                        return Locale.getLocale();
+                    },
+                    function () {
+                        scope.templatePath = './partials/locale/' + Locale.getLanguage() + '/' + attrs.localeTemplate + '.html';
+                    }
+                );
+            },
+            template: '<div data-ng-include="templatePath"></div>'
+        };
+    }])
+
+    .factory('Locale', ['$location', 'localStorageService', function ($location, LocalStorage) {
+        return (function () {
 
             function Locale() {
                 this.availableLocales = [
@@ -77,12 +96,12 @@ angular.module('LocaleModule', ['LocalStorageModule'])
             function checkForBrowserLanguage(Locale) {
                 var browserLanguage, foundLanguage;
                 // Chrome / Firefox
-                if(window.navigator.language) {
+                if (window.navigator.language) {
                     browserLanguage = window.navigator.language.replace('-', '_');
                     foundLanguage = Locale.findLocale(browserLanguage);
                 }
                 // IE
-                if(window.navigator.userLanguage) {
+                if (window.navigator.userLanguage) {
                     browserLanguage = window.navigator.userLanguage.replace('-', '_');
                     foundLanguage = Locale.findLocale(browserLanguage);
                 }
@@ -91,7 +110,7 @@ angular.module('LocaleModule', ['LocalStorageModule'])
 
             function consumeLanguageLink(Locale) {
                 var consumedLanguage = Locale.findLocale($location.search().language);
-                if(consumedLanguage) {
+                if (consumedLanguage) {
                     if ($location.$$search.lang) {
                         delete $location.$$search.lang;
                         $location.$$compose();
@@ -105,23 +124,23 @@ angular.module('LocaleModule', ['LocalStorageModule'])
                 Locale.changeLocale(defaultLocale.value);
             }
 
-            Locale.prototype.getLocale = function() {
+            Locale.prototype.getLocale = function () {
                 return this.activeLocale.value;
             };
 
-            Locale.prototype.getLanguage = function() {
+            Locale.prototype.getLanguage = function () {
                 return this.activeLocale.value.substring(0, 2);
             };
 
-            Locale.prototype.getCountry = function() {
+            Locale.prototype.getCountry = function () {
                 return this.activeLocale.value.substring(3, 5);
             };
 
-            Locale.prototype.findLocale = function(localeShortcut) {
-                var foundSupportedLanguage = _.find(this.availableLocales, function(locale) {
+            Locale.prototype.findLocale = function (localeShortcut) {
+                var foundSupportedLanguage = _.find(this.availableLocales, function (locale) {
                     return locale.value === localeShortcut;
                 });
-                if(foundSupportedLanguage) {
+                if (foundSupportedLanguage) {
                     return foundSupportedLanguage;
                 }
                 else {
@@ -129,30 +148,30 @@ angular.module('LocaleModule', ['LocalStorageModule'])
                 }
             };
 
-            Locale.prototype.changeLocale = function(locale) {
+            Locale.prototype.changeLocale = function (locale) {
                 var foundLocale = this.findLocale(locale);
-                if(foundLocale) {
+                if (foundLocale) {
                     this.activeLocale = foundLocale;
                     LocalStorage.cookie.add('language', foundLocale.value);
                     LocalStorage.add('language', foundLocale.value);
                 }
                 else {
-                    console.log('Locale '+locale+' not available');
+                    console.log('Locale ' + locale + ' not available');
                 }
             };
 
-            Locale.prototype.initLocale = function() {
+            Locale.prototype.initLocale = function () {
                 var self = this;
                 var consumedLanguage = consumeLanguageLink(self);
                 var cookieLanguage = checkForCookie(self);
                 var browserLanguage = checkForBrowserLanguage(self);
-                if(angular.isDefined(consumedLanguage)) {
+                if (angular.isDefined(consumedLanguage)) {
                     this.changeLocale(consumedLanguage.value);
                 }
-                else if(angular.isDefined(cookieLanguage)) {
+                else if (angular.isDefined(cookieLanguage)) {
                     this.changeLocale(cookieLanguage.value);
                 }
-                else if(angular.isDefined(browserLanguage)) {
+                else if (angular.isDefined(browserLanguage)) {
                     this.changeLocale(browserLanguage.value);
                 }
                 else {
@@ -163,9 +182,4 @@ angular.module('LocaleModule', ['LocalStorageModule'])
             return new Locale();
 
         })();
-    }])
-
-
-    .run(['Locale', function (Locale) {
-        Locale.initLocale();
     }]);
