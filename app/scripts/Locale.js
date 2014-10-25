6a0dc70818b39eecaecbfc24e88a09ed0453de78
Malte Bruweleit <bruweleit_malte@team-neotech.de>
2014-10-25 19:15:19 +0200

    fixed local storage service provider

--- app/scripts/Locale.js
+++ app/scripts/Locale.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('LocaleModule', ['LocalStorageModule'], function (LocalStorageProvider) {
-    LocalStorageProvider.setPrefix('');
+angular.module('LocaleModule', ['LocalStorageModule'], function (localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('');
 })
 
 
